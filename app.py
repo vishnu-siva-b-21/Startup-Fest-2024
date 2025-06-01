8bc786df9dbbc38e993f34df216fe7b65c4e8b76
vishnu-siva-b-21 <vishnu.siva.b.21@gmail.com>
2025-06-01 13:10:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from waitress import serve
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # serve(app, host="0.0.0.0", port=8000)
+    # app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8000)
